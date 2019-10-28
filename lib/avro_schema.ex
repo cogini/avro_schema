d0554a91aaf897f7c0b6be122c0394529aed8926
Jake Morrison <jake@cogini.com>
2019-10-28 11:10:02 +0800

    Create project

---
+++ lib/avro_schema.ex
@@ -0,0 +1,18 @@
+defmodule AvroSchema do
+  @moduledoc """
+  Documentation for AvroSchema.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AvroSchema.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/avro_schema_test.exs
@@ -0,0 +1,8 @@
+defmodule AvroSchemaTest do
+  use ExUnit.Case
+  doctest AvroSchema
+
+  test "greets the world" do
+    assert AvroSchema.hello() == :world
+  end
+end
