6183875e9a1315b6ce1bc9aa22e68cca697bfe91
Jake Morrison <jake@cogini.com>
2020-02-18 16:45:03 +0800

    Make 0.3.0 release

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule AvroSchema.MixProject do
   def project do
     [
       app: :avro_schema,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
