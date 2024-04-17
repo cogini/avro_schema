cb2a77ffc93f50af8363dcb184d0314e6b814b3a
Jake Morrison <jake@cogini.com>
2024-04-17 16:58:14 -0500

    Configure JUnit formatter

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,9 +1,10 @@
-{:ok, _pid} = AvroSchema.start_link(cache_dir: "/tmp")
-ExUnit.start()
-
 ExUnit.configure(
+  formatters: [JUnitFormatter, ExUnit.CLIFormatter],
   exclude: [
     :skip,
     :live_registry
   ]
 )
+
+{:ok, _pid} = AvroSchema.start_link(cache_dir: "/tmp")
+ExUnit.start()
